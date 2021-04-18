abc10f3baa05b22722ad258e8afaf97b1c0add43
OLAWALE TOFADE <tofadeolawale@gmail.com>
2021-04-18 22:27:30 +0100

    First Commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! Shutting Down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: "./env" });
+const app = require("./app");
+
+// Use Database connection for hosted database if available or Use Local Database
+const DB =
+  process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD) ||
+  process.env.DATABASE_LOCAL;
+
+//const DB = process.env.DATABASE_LOCAL;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection successfull"));
+
+//START SERVER
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`app running on port ${port}...`);
+  // eslint-disable-next-line prettier/prettier
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("SIGTERM", () => {
+  console.log("👋 SIGTERM RECEIVED. Shutting down gracefully");
+  server.close(() => {
+    console.log("💥 Process terminated!");
+  });
+});
