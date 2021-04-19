4fe820387bd19e33e7d4c61fea635f5e8e885088
OLAWALE TOFADE <tofadeolawale@gmail.com>
2021-04-19 22:47:52 +0100

    refactore test.js

--- controllers/shortnerController.js
+++ controllers/shortnerController.js
@@ -8,47 +8,51 @@ const baseUrl = process.env.BASEURL + ":" + process.env.PORT;
 
 exports.createUrl = catchAsync(async (req, res, next) => {
   const { initialUrl } = req.body; // destructure the initialUrl from req.body.initialUrl
-
-  // check base url if url is valid
-  if (!validUrl.isUri(baseUrl)) {
-    next(new AppError("Invalid base URL", 400));
-  }
-
-  // if valid, we create the url code
-  const urlCode = nanoid(10);
-
-  // check initial url if valid using the validUrl.isUri method
-  if (validUrl.isUri(initialUrl)) {
-    //we check if the initial URL was in the DB ,else we create it.
-    let url = await Shortner.findOne({
-      initialUrl,
-    });
-
-    // url exist and return the respose
-    if (url) {
-      res.json(url);
-    } else {
-      // join the generated short code the the base url
-      const shortUrl = baseUrl + "/" + urlCode;
-
-      // saving to the DB
-      url = new Shortner({
-        initialUrl,
-        shortUrl,
-        urlCode,
-      });
-      await url.save();
-      res.json(url);
+  if (initialUrl) {
+    // check base url if url is valid
+    if (!validUrl.isUri(baseUrl)) {
+      next(new AppError("Invalid base URL", 400));
     }
+
+    // if valid, we create the url code
+    const urlCode = nanoid(10);
+
+    // check initial url if valid using the validUrl.isUri method
+    if (validUrl.isUri(initialUrl)) {
+      //we check if the initial URL was in the DB ,else we create it.
+      let url = await Shortner.findOne({
+        initialUrl,
+      });
+
+      // url exist and return the respose
+      if (url) {
+        res.json(url);
+      } else {
+        // join the generated short code the the base url
+        const shortUrl = baseUrl + "/" + urlCode;
+
+        // saving to the DB
+        url = new Shortner({
+          initialUrl,
+          shortUrl,
+          urlCode,
+        });
+        await url.save();
+        res.json(url);
+      }
+    } else {
+      return new AppError("Invalid InitialUrl", 400);
+      next();
+    }
+    res.status(201).json({
+      status: "success",
+      data: {
+        data: url,
+      },
+    });
   } else {
-    return new AppError("Invalid InitialUrl", 400);
+    next(new AppError("InitialUrl is required", 400));
   }
-  res.status(201).json({
-    status: "success",
-    data: {
-      data: url,
-    },
-  });
 });
 
 exports.redirect = catchAsync(async (req, res) => {
@@ -61,6 +65,6 @@ exports.redirect = catchAsync(async (req, res) => {
     return res.redirect(url.initialUrl);
   } else {
     // else return a not found 404 status
-    return new AppError("Invalid InitialUrl", 400);
+    return new AppError("Invalid shortUrl", 400);
   }
 });
--- test.js
+++ test.js
@@ -1,13 +1,15 @@
 /* prettier-ignore-file */
 //process.env.NODE_ENV = 'development';
 
-let mongoose = require("mongoose");
-let Shortner = require("./models/shortnerModel");
+const mongoose = require("mongoose");
+const Shortner = require("./models/shortnerModel");
 
-let chai = require("chai");
-let chaiHttp = require("chai-http");
-let app = require("./app");
-let should = chai.should();
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("./app");
+const server = require("./server");
+//const should = chai.should();
+const should = require("should");
 
 chai.use(chaiHttp);
 
@@ -16,116 +18,64 @@ describe("Shortner", () => {
     it("it should not POST a URL without initialUrl field", async () => {
       let shortner = "http://jjj.com";
       chai
-        .request(app)
+        .request(server)
         .post("/api/v1/shortner")
         .send(shortner)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a("object");
-          res.body.should.have.property("errors");
-          res.body.errors.should.have.property("initialUrl");
-          res.body.errors.pages.should.have.property("kind").eql("required");
+        .end((res) => {
+          should(res.status).be.equal(400);
+          should(res.body).be.a("object");
+          should(res.body).have.property("errors");
+          // should(res.body).errors.have.property("initialUrl");
+          // should(res.body).errors.pages.have.property("kind").eql("required");
           done();
         });
       //.catch(done);
     });
     it("it should CREATE a shortUrl ", async () => {
       let shortner = {
-        initialUrl: "https://github.com/ai/nanoid/",
+        initialUrl: "https://github.com/ai/nanoid",
       };
       chai
-        .request(app)
+        .request(server)
         .post("/api/v1/shortner")
         .send(shortner)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a("object");
-          res.body.should.have.property("status").eql("success");
-          res.body.shortner.should.have.property("initialUrl");
+        .end((res) => {
+          should.exist(res.body);
+          should(res.status).be.equal(200);
+          should(res.body).be.a("object");
+          should(res.body).shortner.have.property("shortUrl");
+          should(res.body).shortner.have.property("urlCode");
+          done();
+        });
+    });
+
+    it("it should not allow invalid url ", async () => {
+      let shortner = {
+        initialUrl: "kkkk",
+      };
+      chai
+        .request(server)
+        .post("/api/v1/shortner")
+        .send(shortner)
+        .end((res) => {
+          should(res.status).be.equal(400);
+          //should(res.body).have.property("error");
           done();
         });
     });
   });
-  // describe('/GET/:id book', () => {
-  //   it('it should GET a book by the given id', (done) => {
-  //     let book = new Book({
-  //       title: 'The Lord of the Rings',
-  //       author: 'J.R.R. Tolkien',
-  //       year: 1954,
-  //       pages: 1170,
-  //     });
-  //     book.save((err, book) => {
-  //       chai
-  //         .request(server)
-  //         .get('/book/' + book.id)
-  //         .send(book)
-  //         .end((err, res) => {
-  //           res.should.have.status(200);
-  //           res.body.should.be.a('object');
-  //           res.body.should.have.property('title');
-  //           res.body.should.have.property('author');
-  //           res.body.should.have.property('pages');
-  //           res.body.should.have.property('year');
-  //           res.body.should.have.property('_id').eql(book.id);
-  //           done();
-  //         });
-  //     });
-  //   });
-  // });
-  // describe('/PUT/:id book', () => {
-  //   it('it should UPDATE a book given the id', (done) => {
-  //     let book = new Book({
-  //       title: 'The Chronicles of Narnia',
-  //       author: 'C.S. Lewis',
-  //       year: 1948,
-  //       pages: 778,
-  //     });
-  //     book.save((err, book) => {
-  //       chai
-  //         .request(server)
-  //         .put('/book/' + book.id)
-  //         .send({
-  //           title: 'The Chronicles of Narnia',
-  //           author: 'C.S. Lewis',
-  //           year: 1950,
-  //           pages: 778,
-  //         })
-  //         .end((err, res) => {
-  //           res.should.have.status(200);
-  //           res.body.should.be.a('object');
-  //           res.body.should.have.property('message').eql('Book updated!');
-  //           res.body.book.should.have.property('year').eql(1950);
-  //           done();
-  //         });
-  //     });
-  //   });
-  // });
-  // /*
-  //  * Test the /DELETE/:id route
-  //  */
-  // describe('/DELETE/:id book', () => {
-  //   it('it should DELETE a book given the id', (done) => {
-  //     let book = new Book({
-  //       title: 'The Chronicles of Narnia',
-  //       author: 'C.S. Lewis',
-  //       year: 1948,
-  //       pages: 778,
-  //     });
-  //     book.save((err, book) => {
-  //       chai
-  //         .request(server)
-  //         .delete('/book/' + book.id)
-  //         .end((err, res) => {
-  //           res.should.have.status(200);
-  //           res.body.should.be.a('object');
-  //           res.body.should.have
-  //             .property('message')
-  //             .eql('Book successfully deleted!');
-  //           res.body.result.should.have.property('ok').eql(1);
-  //           res.body.result.should.have.property('n').eql(1);
-  //           done();
-  //         });
-  //     });
-  //   });
-  // });
+  describe("/GET/:code shortner", () => {
+    it("it should redirect", async () => {
+      let shortUrl = "http://localhost:3000/A1FYOZtOUo";
+
+      chai
+        .request(server)
+        .get(shortUrl)
+        .send()
+        .end((res) => {
+          should(res.status).be.equal(200);
+          done();
+        });
+    });
+  });
 });
